b469296abcc71716377a1829e8dc4f29ed25188b
u0970346 <weidraw@yahoo.com>
2021-11-01 17:02:03 -0600

    adjust homepage

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,8 +1,17 @@
 function darkmodeFunc() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+
     var elements = document.getElementsByClassName('h5');
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.color = 'black';
+    }
+    
+    var icon = document.getElementById("icon");
+    if(document.body.classList.contains("dark-mode")){
+        icon.src = "/static/images/sun.png";
+    }
+    else{
+        icon.src = "/static/images/moon.png";
     }
 }
